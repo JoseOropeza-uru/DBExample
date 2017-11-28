d83a85501b226a16020b0edc9065766bef59de2a
JoseOropeza-uru <joseoropeza@uru.edu>
2017-11-28 17:00:11 -0400

    Explicacion de promesa agragada

--- public/js/app.js
+++ public/js/app.js
@@ -12,22 +12,6 @@ function sendGet(){
         console.log('Value is null');
     }
 };
-function a (){
- let value = $('promInp').value.trim();
-    b(value).then((data)=>{
-    console.log(`mensaje ${data} enviada desde promesa B`);
- }).catch((errorData)=>{
-    console.log(`${errorData} enviada de error desde promesa B`);
- });
-}
-function b (msg){
-    return new Promise((res,rej)=>{
-        if(msg.trim()==='')
-            rej(msg);
-        else
-            res(msg);
-    });
-}
 function sendPost(){
     if($('ced').value.trim()!==''&&$('name').value.trim()!==''&&$('lastName').value.trim()!==''&&
     $('age').value.trim()!==''){
@@ -44,6 +28,31 @@ function sendPost(){
         console.error('Complete los campos');
     } 
 };
+/*  Ejemplo basico de una promesa
+    donde tenemos una funcion a, que llama a una funcion b;
+    esta funcion b es la definicion de la promesa, la cual recibe un parametro 'msg'
+    el cual va a verificar si esta vacio o no.
+    
+    En caso de que este sea distinto de '' la promesa hace resolve lo que lleva al
+    then dentro de la funcion a, de lo contrario hace reject que lleva al catch
+    dentro de la funcion a. 
+*/
+function a (){
+ let value = $('promInp').value.trim();
+    b(value).then((data)=>{
+    console.log(`mensaje ${data} enviada desde promesa B`);
+ }).catch((errorData)=>{
+    console.log(`${errorData} enviada de error desde promesa B`);
+ });
+}
+function b (msg){
+    return new Promise((res,rej)=>{
+        if(msg.trim()==='')
+            rej(msg);
+        else
+            res(msg);
+    });
+}
 $('promBut').addEventListener('click',a);
 $('butGet').addEventListener('click',sendGet);
 $('butPost').addEventListener('click',sendPost);
